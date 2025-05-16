613755669707bf019fa6bd26f036c9307a3486db
bachxuantrann <bxt203@gmail.com>
2025-05-16 16:37:13 +0700

    123

---
+++ src/main/java/com/springrest/restApp/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.springrest.restApp.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration {
+//    Ma hoa mat khau user
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
