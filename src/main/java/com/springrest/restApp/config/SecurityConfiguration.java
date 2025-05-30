ba09f22a93ec19043933789ae557469f7a25643f
bachxuantrann <bxt203@gmail.com>
2025-05-30 15:00:52 +0700

    bai57

--- src/main/java/com/springrest/restApp/config/SecurityConfiguration.java
+++ src/main/java/com/springrest/restApp/config/SecurityConfiguration.java
@@ -1,18 +1,33 @@
 package com.springrest.restApp.config;
 
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+import com.springrest.restApp.util.SecurityUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
+    @Value("${bxt.jwt.base64-secret}")
+    private String jwtKey;
+
 //    Ma hoa mat khau user
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -24,12 +39,34 @@ public class SecurityConfiguration {
                 .csrf(c->c.disable())
                 .authorizeHttpRequests(
                         authz ->authz
-                                .requestMatchers("/").permitAll()
-                                .anyRequest()
-                                .permitAll()
+                                .requestMatchers("/","/login").permitAll()
+                                .anyRequest().authenticated()
+
                 )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .formLogin(f->f.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(
+                getSecretKey()).macAlgorithm(SecurityUtil.JWT_ALGORITHM).build();
+        return token -> {
+            try{
+                return jwtDecoder.decode(token);
+            } catch (Exception e){
+                System.out.println(">>> JWT error: "+ e.getMessage());
+                throw e;
+            }
+        };
+    }
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtKey).decode();
+        return new SecretKeySpec(keyBytes,0,keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
     }
 }
--- src/main/java/com/springrest/restApp/controller/AuthController.java
+++ src/main/java/com/springrest/restApp/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.springrest.restApp.controller;
 
 
 import com.springrest.restApp.domain.dto.LoginDTO;
+import com.springrest.restApp.domain.dto.ResLoginDTO;
+import com.springrest.restApp.util.SecurityUtil;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,14 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+    private final SecurityUtil securityUtil;
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityUtil =  securityUtil;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDTO){
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO){
+//        get user from login
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
+//        authenticate user
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return ResponseEntity.ok().body(loginDTO);
+//        create token
+        String access_token =  this.securityUtil.createToken(authentication);
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(access_token);
+        return ResponseEntity.ok().body(res);
     }
 }
