29f9989de4c4a96732aec99b11aa3b09a2dd8f4d
bachxuantrann <bxt203@gmail.com>
2025-05-31 17:37:33 +0700

    create model company

---
+++ src/main/java/com/springrest/restApp/domain/Company.java
@@ -0,0 +1,29 @@
+package com.springrest.restApp.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@Setter
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+    private String address;
+    private String logo;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+}
