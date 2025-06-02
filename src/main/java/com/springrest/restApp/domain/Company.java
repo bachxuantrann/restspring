52e93651a0e4784271be7529d82e98fdf4f671ec
bachxuantrann <bxt203@gmail.com>
2025-06-02 11:10:17 +0700

    bai 70 before save company

--- src/main/java/com/springrest/restApp/domain/Company.java
+++ src/main/java/com/springrest/restApp/domain/Company.java
@@ -1,11 +1,14 @@
 package com.springrest.restApp.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.springrest.restApp.util.SecurityUtil;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.Instant;
 
@@ -24,8 +27,18 @@ public class Company {
     private String description;
     private String address;
     private String logo;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant createdAt;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @PrePersist
+    public void handleBeforeCreate(){
+//        this.createdBy = SecurityContextHolder.getContext().getAuthentication().getName();
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true ? SecurityUtil.getCurrentUserLogin().get() : "";
+        this.createdAt = Instant.now();
+
+    }
 }
